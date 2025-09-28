1cae6377ad1664c252bfdb692d51ec0c31177c96
Felipe Eliseu <felipe.eliseu@unesp.br>
2025-09-28 12:42:28 -0300

    fix...

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,7 +6,7 @@ router.get('/', (_req, res) => {
     res.json({ version: '1' });
 });
 
-router.use('/users', (await import('./routers/userRoutes')).default);
-router.use('/session', (await import('./routers/sessionRoutes')).default);
+router.use('/users', (await import('./routers/userRoutes.js')).default);
+router.use('/session', (await import('./routers/sessionRoutes.js')).default);
 
 export default router;
