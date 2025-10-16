e40e05bc334123acf731b0fd406029a6401bb156
Felipe Eliseu <felipe.eliseu@unesp.br>
2025-10-15 23:32:22 -0300

    fix(categories): returns key in endpoints

--- src/api/controllers/categoriesController.ts
+++ src/api/controllers/categoriesController.ts
@@ -5,7 +5,7 @@ export const getAllCategories = async (req, res) => {
     const categories = await prisma.categories.findMany({
       select: { id: true, name: true, createdAt: true, updatedAt: true },
     });
-    res.json(categories);
+    res.json({ categories });
   } catch (error) {
     res.status(500).json({ error: "Failed to fetch categories", errorDetails: error });
   }
@@ -109,7 +109,7 @@ export const getCategoryById = async (req, res) => {
     if (!category) {
       return res.status(404).json({ error: "Category not found" });
     }
-    res.status(200).json(category);
+    res.status(200).json({ category });
   } catch (error) {
     res.status(500).json({ error: "Failed to fetch category", errorDetails: error });
   }
